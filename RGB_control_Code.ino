4fdfceede4b77cbf1e64f15cf20a7f706a8fe92a
Dacts <46546219+Dacts@users.noreply.github.com>
2019-02-01 09:51:31 +0530

    Update and rename Esp_control_using_Dacts.ino to RGB_control_Code.ino

--- RGB_control_Code.ino
+++ RGB_control_Code.ino
@@ -1,45 +1,13 @@
 /* 
-App connection instruction:
-Step 1 :
-1.Open the app and create a new project.Note the auth key in project menu page.
-2.Note the ip address of the Mobile which is running Dacts : mqtt server.
-3.Create buttons or slider by clicking Add Device button in home page.
-4.If you create PinButton named as D1 the output from the server wil be {"D1":1} or {"D1":0} (ON or OFF respectively).
-For Rgb Slider the output will be :
-{"Rgb_name":{"r":10,"g":20,"b":30}}
-For Slider the output will be :
-{"Slider_name":555}
-For imageButton the output will be:
-{"image_name":0} or {"image_name":1} (OFF or ON)
-......................................................
-
-Step 2 :
-1.Edit auth below.
-2.Edit wifiname and wifipassword
-3.Edit ip address.
-4.Edit port. below.
-
-......................................................
-Step 3 :
-1.Open the arduino ide and paste the below code .
-2 Edit the code in setup PinMode .
-3.Edit the code after json phrase with your name of button or slider. 
-4.Edit delay as you want.
-5.Update the code to Esp8266 or your arduino.
-.................................................................
-
-IMPORTANT NOTE:
-Both the mobile and devboards should be connected to the same network either 
-your Router or to your mobile hotspot.
-Because it runs a local server.
-
-Another Note:
-Make sure your Ide have the required library for your boards.
-Below code is for Node Mcu8266.
-The library used :
-1.Esp8266
-2.Arduino Json 
-
+1.Create A rgb slider in your project with name Test RGB
+2.Now edit the ssid , password 
+3.Change the userkey to your prokect user key .(line 23. )
+4.Change ip address.( line 22)
+5.Connect RGb led as follow :
+PIN D2 as Red
+PIN D3 as Green
+PIN D4 as Red
+now upload.
 */
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h> 
@@ -52,14 +20,14 @@ char* wifiName = "Your Wifi Name";
 char* wifiPass = "Your wifi password";
 
 String ip = "192.168.1.3";//Ip address of android device running Dacts
-String auth="Athp21R1aW1917I40N38mu296Oth345";//your auth key in app
+String userkey="Athp21R1aW1917I40N38mu296Oth345";//your user key in app
 int port = 8080;// port number in settings in the app
 String output;
 void setup(){
   pinMode(D2,OUTPUT); // Rgb -- red
   pinMode(D3,OUTPUT); // Rgb -- green
   pinMode(D4,OUTPUT); // Rgb -- blue
-  pinMode(D5,OUTPUT); // Slider value
+  
   WiFi.mode(WIFI_OFF);        //Prevents reconnection issue 
   delay(1000);
   WiFi.mode(WIFI_STA);
@@ -86,17 +54,17 @@ void loop(){
           int r = root["Test RGB"]["r"];//name you registered for the rgb light here "Test RGB" is used change the name
           int g = root["Test RGB"]["g"];
           int b = root["Test RGB"]["b"];
-          int slider = root["Pwm SLider"];
-          analogWrite(D2,r*4);
-          analogWrite(D3,g*4);
-          analogWrite(D4,b*4);
-          analogWrite(D5,slider); // slider value 0 to 1024
+          
+          analogWrite(D2,r*4); // multiplied by 4 because the value will be of range 0 -255 to increase
+          analogWrite(D3,g*4); // brightness it is multiplied by 4.
+          analogWrite(D4,b*4); // 
+          
           delay(200);
 
 }
 String serverData(){
   String out;
-  String link = "http://"+ip+":"+port+"/?auth="+auth;
+  String link = "http://"+ip+":"+port+"/?auth="+userkey;
     http.begin(link);
     int Code = http.GET();
      if (Code > 0) {
